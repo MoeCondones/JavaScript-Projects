ab1deb9712ea3e85bd019ca5adf27991ba862d10
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2022-10-18 11:56:36 +0100

    Add files via upload

--- Basic JavaScript Projects/Project1_expressions_alert/js/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -1,1 +1,10 @@
-document.write("let's go out and eat. but is the pizza place open?")
+window.alert("yo");
+let firstName = "Andy "; //firstname is assigned andy
+let lastName = "Warhol ";//lastname assigned warhol
+let warhol = lastName.fontcolor("blue");//color of lastname
+document.write(firstName);//write firstname
+document.write(warhol)//write last name
+
+let bio = "Andy Warhol was an American visual artist, "+"film director, and producer who was a leading figure in the visual art movement known as pop art ";//assigning bio
+document.write(bio);//pringint bio
+document.write(3+3)//printing an expression

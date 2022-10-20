7bd127df076df3eb7af9e867ac1750f54d60823e
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2022-10-20 13:48:00 +0100

    Add files via upload

---
+++ Basic JavaScript Projects/Project3_math_operators folder/js/main.js
@@ -0,0 +1,21 @@
+function addFunc(){
+    var add = 38 + 10;
+    document.getElementById("add").innerHTML="38 + 10="+add;//addition of 38+10
+}
+
+function subFunc(){
+    var sub = 25 - 5;
+    document.getElementById("sub").innerHTML="25 - 5="+sub;//substraction of 25-25
+}
+
+function multiFunc(){
+    var multi = 8 * 3;
+    document.getElementById("multi").innerHTML="8 * 3="+multi//multiplication of 8*3
+}
+
+function divFunc(){ 
+    var div = 10 / 5;
+    document.getElementById("div").innerHTML="10 / 5="+div//divition of 10/5
+}
+
+document.write(Math.random())//generate a random number

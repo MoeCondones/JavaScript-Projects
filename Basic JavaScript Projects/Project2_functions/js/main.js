2466f06f6141d12a525cea98b8c2ef96e4e2968e
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2022-10-20 11:36:29 +0100

    Add files via upload

--- Basic JavaScript Projects/Project2_functions/js/main.js
+++ Basic JavaScript Projects/Project2_functions/js/main.js
@@ -1,5 +1,19 @@
-function myFirstFunc() {
-    var sentence = "im learning ";
-    sentence += "a lot from this javascript course";
-    document.getElementById("concatenate").innerHTML=sentence
+function addFunc(){
+    var addition = 5 + 3;
+    document.getElementById("add").innerHTML="5 + 3="+addition;
+}
+
+function subFunc(){
+    var sub = 5 - 3;
+    document.getElementById("sub").innerHTML="5 - 3="+sub
+}
+
+function multiFunc(){
+    var mult = 5 * 3;
+    document.getElementById("Multi").innerHTML="5 * 3="+mult
+}
+
+function divFunc(){
+    var sub = 10 / 2;
+    document.getElementById("div").innerHTML="10 / 2="+sub
 }

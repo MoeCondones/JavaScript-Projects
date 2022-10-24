eef4939293fb4129b76f6f55fb0084626a8a34e9
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2022-10-24 12:05:20 +0100

    Add files via upload

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/js/main.js
@@ -0,0 +1,24 @@
+let x = 5*2;
+math=5*2;
+document.write(typeof x);
+
+document.write("10" +5);
+
+
+y=10;
+z=10;
+document.write(y==z);
+
+num1=23;
+num2=10;
+document.write(num1===num2);
+
+document.write(5>2);
+
+document.write(102<293);
+
+document.write(25>10&&23>49)
+
+document.write(10>3||10>12)
+
+document.getElementById("not").innerHTML=!(20>10)

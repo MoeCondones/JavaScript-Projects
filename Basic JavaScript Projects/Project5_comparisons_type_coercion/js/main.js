96ba074b56ee7c3f6663b9149220650420b01d58
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2022-10-24 12:11:12 +0100

    Add files via upload

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/js/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/js/main.js
@@ -1,24 +1,24 @@
-let x = 5*2;
-math=5*2;
-document.write(typeof x);
+let x = 5*2; //setting a value for x
+math=5*2; // setting a value for math
+document.write(typeof x); //printing typeof x
 
-document.write("10" +5);
+document.write("10" +5); //pringing the string and number expression
 
 
-y=10;
-z=10;
-document.write(y==z);
+y=10; //setting a value for y
+z=10; //setting a value for z
+document.write(y==z); //printing to see if y is equal to z
 
-num1=23;
-num2=10;
-document.write(num1===num2);
+num1=23; //setting a value to num1
+num2=10; // setting a value to num2
+document.write(num1===num2); //printing to see if num1 is equal to num2
 
-document.write(5>2);
+document.write(5>2); //printing to see if 5 is greater than 2
 
-document.write(102<293);
+document.write(102<293); // printing to see if 102 is less than 293
 
-document.write(25>10&&23>49)
+document.write(25>10&&23>49) //printing to see if 25>10 AND 23>49
 
-document.write(10>3||10>12)
+document.write(10>3||10>12) //printing to see if 10>3 OR 10>12
 
-document.getElementById("not").innerHTML=!(20>10)
+document.getElementById("not").innerHTML=!(20>10) //printing to see if 20>10 if yes itll print false if no itll print true

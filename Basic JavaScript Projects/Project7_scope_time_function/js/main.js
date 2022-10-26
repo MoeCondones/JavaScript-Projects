8e74de13bc9f3a811a70290a3294b9850d1a5b28
MoeCondones <95757662+MoeCondones@users.noreply.github.com>
2022-10-26 15:35:32 +0100

    Add files via upload

--- Basic JavaScript Projects/Project7_scope_time_function/js/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/js/main.js
@@ -1,31 +1,31 @@
-x=10
-function globalFunc(){
-    document.write(20*x+"<br>")
+x=10// assigning a value to x
+function globalFunc(){//creating a function 
+    document.write(20*x+"<br>")//printing 20*x
 }
-function globalFunc1(){
-    document.write(12-x+"<br>")
+function globalFunc1(){//creating a new function
+    document.write(12-x+"<br>")//printing 12-x
 }
-globalFunc();
-globalFunc1();
+globalFunc();//calling the functions
+globalFunc1();//calling the functions
 
-y=25
-z=50
-function ifFunc(){
-    if (y<z){
-        console.log("25 is less than 50 ")
+y=25//assigning a value to y
+z=50// assigning a vlaue to z
+function ifFunc(){//creating a new function
+    if (y<z){//creating an if steatement
+        console.log("25 is less than 50 ")//printing the result
     }
-    else{
-        console.log("25 is not bigger than 50")
+    else{//creating an else statement
+        console.log("25 is not bigger than 50")//printing the result of the else statement
     }
 }
 
-function func1(){
-    var k = 10
-    document.write(20+k);
+function func1(){//creating a new function
+    var k = 10//assigning a value to k
+    document.write(20+k);//printing 20+k
 }
 
-function func2(){
-    document.write(29*k);
+function func2(){//creating a new function
+    document.write(29*k);//printing 29*k
 }
-func1();
-func2();
+func1();//calling func1
+func2();// calling func2
